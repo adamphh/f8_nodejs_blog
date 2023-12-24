b301b7d06a649ec9f623b3318161ff627cc311ec
Adam Pham <adam@magestore.com>
2023-12-25 00:56:46 +0700

    Using template engine with static file and scss, bootstrap4

---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require('morgan');
+const { engine } = require('express-handlebars');
+const app = express();
+const port = 3000;
+const path = require('path');
+
+// Static content 
+app.use(express.static(path.join(__dirname, 'public')))
+
+// HTTP logger
+app.use(morgan('combined'));
+
+// Template engine
+app.engine('hbs', engine({
+    extname: 'hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+/** route */
+app.get('/', (req, res) => {
+    res.render('home', { layout: 'home' });
+})
+app.get('/home', (req, res) => {
+    res.send('home page')
+})
+
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
